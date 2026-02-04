44149109f38b3040e057467b911dd8b1cd83f11d
Lakshmi Murugappan <lakshmi.murugappan@tngtech.com>
2026-02-04 14:13:43 +0000

    Add unarchive details to archive reasons
    Allows us to track how, by whom and why a record was unarchived to shows this on the patients activity log

--- db/schema.rb
+++ db/schema.rb
@@ -49,12 +49,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_29_175616) do
     t.bigint "patient_id", null: false
     t.bigint "team_id", null: false
     t.integer "type", null: false
+    t.integer "unarchive_reason"
+    t.datetime "unarchived_at"
+    t.bigint "unarchived_by_user_id"
     t.datetime "updated_at", null: false
     t.index ["created_by_user_id"], name: "index_archive_reasons_on_created_by_user_id"
     t.index ["patient_id", "team_id"], name: "index_archive_reasons_on_patient_id_and_team_id", unique: true
     t.index ["patient_id"], name: "index_archive_reasons_on_patient_id"
     t.index ["team_id", "patient_id"], name: "index_archive_reasons_on_team_id_and_patient_id", unique: true
     t.index ["team_id"], name: "index_archive_reasons_on_team_id"
+    t.index ["unarchived_by_user_id"], name: "index_archive_reasons_on_unarchived_by_user_id"
   end
 
   create_table "attendance_records", force: :cascade do |t|
@@ -1093,6 +1097,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_29_175616) do
   add_foreign_key "archive_reasons", "patients"
   add_foreign_key "archive_reasons", "teams"
   add_foreign_key "archive_reasons", "users", column: "created_by_user_id"
+  add_foreign_key "archive_reasons", "users", column: "unarchived_by_user_id"
   add_foreign_key "attendance_records", "locations"
   add_foreign_key "attendance_records", "patients"
   add_foreign_key "batches", "teams"
